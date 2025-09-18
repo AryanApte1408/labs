c24b8714d7ecdaa53239b20e6c91c8f7c160943e
Aryan Apte <aryan.kindle9000@gmail.com>
2025-09-18 11:01:47 -0400

    lab4 patched

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
+# # # import streamlit as st
+
+# # st.set_page_config(page_title="Multi-Page Labs", page_icon="🔬", layout="centered")
+
+# # # Put Lab 2 first so it opens by default
+# # nav = st.navigation([
+# #     st.Page("lab2.py", title="Lab 2"),
+# #     st.Page("lab1.py", title="Lab 1"),
+# # ])
+
+# # nav.run()
+
+
+# # streamlit_app.py
 # import streamlit as st
 
 # st.set_page_config(page_title="Multi-Page Labs", page_icon="🔬", layout="centered")
@@ -5,16 +19,22 @@
 # nav = st.navigation([
 #     st.Page("lab2.py", title="Lab 2 — Weighted PDF Summarizer"),
 #     st.Page("lab1.py", title="Lab 1 — Document Buddy"),
+# <<<<<<< HEAD
+#     st.Page("lab3.py", title="Lab 3 — Chatbot"),   # ✅ new page
+# =======
+# >>>>>>> 7a22ec9e847bd4a6cf259d5e77afb07559763151
 # ])
 
 # nav.run()
 
-# streamlit_app.py
+
 import streamlit as st
 
 st.set_page_config(page_title="Multi-Page Labs", page_icon="🔬", layout="centered")
 
 nav = st.navigation([
+    st.Page("lab4.py", title="Lab 4 — ChromaDB"),
+    st.Page("lab3.py", title="Lab 3 — Chatbot"),
     st.Page("lab2.py", title="Lab 2 — Weighted PDF Summarizer"),
     st.Page("lab1.py", title="Lab 1 — Document Buddy"),
     st.Page("lab3.py", title="Lab 3 — Chatbot"),   # ✅ new page
